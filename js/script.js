bc1f75508ed8577cd2a4c3dc4ae8107ae4cff8b8
wanjiiru <liz240@live.com>
2018-05-10 02:42:21 -0500

    add play functions to js filr

--- js/script.js
+++ js/script.js
@@ -9,3 +9,14 @@ var player2 = "";
 var throw = function(){
 	return Math.floor(6*math.random())+1;
 }
+
+
+function player(turn){
+	this.roll=0;
+	this.playerScore=0;
+	this.totalScore=0;
+	this.turn= turn;
+	this.playerName;
+}
+
+
